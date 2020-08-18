41817ddc5146e77db51279938d0ae6ea15f46faf
NLSanyu <lydiansanyu@gmail.com>
2020-08-18 20:00:09 +0300

    Add query params to filter posts

--- posts.py
+++ posts.py
@@ -8,10 +8,19 @@ group_list = config('GROUP_LIST_ID')
 db_client = pymongo.MongoClient()
 posts_db = db_client["fbposts"]
 col = posts_db["posts"]
+posts_db.posts.create_index([('text', pymongo.ASCENDING)], unique=True)
 
-payload = {'token': api_token, 'listIds': group_list}
+payload = {
+    'token': api_token, 'listIds': group_list,
+    'startDate': '2020-03-01', 'sortBy': 'date'
+}
 url = 'https://api.crowdtangle.com/posts'
 posts_data = requests.get(url, params=payload)
-posts = posts_data.json()['result']
+posts = posts_data.json()['result']['posts']
 
-col.insert_many(posts['posts'])
+post_text_list = []
+for post in posts:
+    text = post.get('message', '')
+    post_text_list.append({'text': text})
+
+col.insert_many(post_text_list)
