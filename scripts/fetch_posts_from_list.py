482e5d32483972d0bfe720a7906a8117ec0b33a8
NLSanyu <lydiansanyu@gmail.com>
2020-11-19 18:40:00 +0300

    Put database and collection names into env file

--- scripts/fetch_posts_from_list.py
+++ scripts/fetch_posts_from_list.py
@@ -3,13 +3,15 @@ import requests
 from utils import get_db_collection
 
 
-def fetch_posts():
+def fetch_posts(api_token, url):
     params = {
-        'token': api_token, 'listIds': group_list,
-        'startDate': '2020-03-01', 'sortBy': 'date',
+        'token': api_token,
+        'listIds': group_list,
+        'startDate': '2020-11-15',
+        'sortBy': 'date',
         'count': 99
     }
-    url = 'https://api.crowdtangle.com/posts'
+
     posts_data = requests.get(url, params=params)
     posts = posts_data.json()['result']['posts']
 
@@ -20,13 +22,16 @@ def fetch_posts():
     return posts_text
 
 
-def insert_posts_in_db(posts):
-    posts_collection = get_db_collection('fb_posts', 'moh-posts')
+def insert_posts_in_db(posts, db_name, collection_name):
+    posts_collection = get_db_collection(db_name, collection_name)
     posts_collection.insert_many(posts)
 
 
 if __name__ == '__main__':
     api_token = config('CROWDTANGLE_API_TOKEN')
     group_list = config('MOH_GROUP_LIST_ID')
-    posts = fetch_posts()
-    insert_posts_in_db(posts)
+    db = config('DB_NAME')
+    col = config('MOH_DB_COLLECTION_NAME')
+    url = 'https://api.crowdtangle.com/posts'
+    posts = fetch_posts(api_token, url)
+    insert_posts_in_db(posts, db, col)
--- scripts/retrieve_posts_from_db.py
+++ scripts/retrieve_posts_from_db.py
@@ -1,5 +1,15 @@
-import pymongo
+from decouple import config
+from utils import get_db_collection
 
-db_client = pymongo.MongoClient()
-posts_db = db_client["fbposts"]
-col = posts_db["covid-posts"]
+
+def retrieve_posts(collection, query={}):
+    posts = collection.find(query)
+    return posts
+
+
+if __name__ == '__main__':
+    db = config('DB_NAME')
+    col = config('MOH_DB_COLLECTION_NAME')
+    posts_collection = get_db_collection(db, col)
+    posts = retrieve_posts(posts_collection)
+    print(posts.collection)
